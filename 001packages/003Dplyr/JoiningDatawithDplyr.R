51189dc101565c115808179e1d0fc5cac0608bcc
Lockyar <1043293+Lockyar@users.noreply.github.com>
2019-03-12 12:20:14 -0500

    Joining Data with Dyplr

--- 001packages/003Dplyr/JoiningDatawithDplyr.R
+++ 001packages/003Dplyr/JoiningDatawithDplyr.R
@@ -1,6 +1,7 @@
 # Joinning Data with Dplyr
 library(dplyr)
 
+# Mutating Joins
 # Complete the code to join artists to bands
 # left_join(x, y) joins y to x. The second dataset you specify is joined to the first dataset. 
 bands2 <- left_join(bands, artists, by = c("first", "last"))
@@ -51,4 +52,389 @@ artists %>%
   full_join(bands, by = c("first", "last")) %>%
   full_join(songs, by = c("first", "last")) %>%
   full_join(albums, by = c("album", "band"))
+
+# Filtering Joins
+# View the output of semi_join()
+# As you saw in the video, semi-joins provide a concise way to filter data from the first dataset based on information in a second dataset.
+artists %>% 
+  semi_join(songs, by = c("first", "last"))
+
+# Create the same result
+artists %>% 
+  right_join(songs, by = c("first", "last")) %>% 
+  filter(!is.na(instrument)) %>% 
+  select(first, last, instrument)
+
+albums %>% 
+  # Collect the albums made by a band
+  semi_join(bands, by = "band") %>% 
+  # Count the albums made by a band
+  nrow()
+
+# anti-joins provide a useful way to reason about how a mutating join will work before you apply the join.
+# Return rows of artists that don't have bands info
+artists %>% 
+  anti_join(bands, by = c("first", "last"))
+
+# Check whether album names in labels are mis-entered
+labels %>% 
+  anti_join(albums, by = "album")
+
+# Check your understanding
+songs %>% 
+  # Find the rows of songs that match a row in labels
+  semi_join(labels, by = "album") %>% 
+  # Number of matches between labels and songs
+  nrow()
+
+# Set Operations
+# union(), intersect(), setdiff()
+aerosmith %>% 
+  # Create the new dataset using a set operation
+  union(greatest_hits) %>% 
+  # Count the total number of songs
+  nrow()
+
+# Create the new dataset using a set operation
+aerosmith %>% 
+  intersect(greatest_hits)
+
+# Select the song names from live
+live_songs <- live %>% select(song)
+
+# Select the song names from greatest_hits
+greatest_songs <- greatest_hits %>% select(song)
+
+# Create the new dataset using a set operation
+live_songs %>% 
+  setdiff(greatest_songs)
+
+# Select songs from live and greatest_hits
+live_songs <- live %>% select(song)
+greatest_songs <- greatest_hits %>% select(song)
+
+
+# Find songs in at least one of live_songs and greatest_songs
+all_songs <- greatest_songs %>% union(live_songs)
+
+# Find songs in both 
+common_songs <- greatest_songs %>% intersect(live_songs)
+
+# Find songs that only exist in one dataset
+all_songs %>% setdiff(common_songs)
+
+# indentical() and setequal()
+# They are different because identical() must have the same order, setequal() just compares the data.
+# Check if same order: definitive and complete
+identical(definitive, complete)
+
+# Check if any order: definitive and complete
+setequal(definitive, complete)
+
+# Songs in definitive but not complete
+setdiff(definitive, complete)
+
+# Songs in complete but not definitive
+setdiff(complete, definitive)
+
+# When your datasets contain the same variables, a setdiff() does the same thing as an anti_join() that uses every column as a key.
+# Return songs in definitive that are not in complete
+definitive %>% 
+  anti_join(complete, by = c("song", "album"))
+
+# Return songs in complete that are not in definitive
+complete %>% 
+  anti_join(definitive, by = c("song", "album"))
+
+# Get union of complete and soundtrack
+complete_and_soundtrack <- complete %>% union(soundtrack)
+head(complete_and_soundtrack)
+
+# Check if same, including order: definitive and union of complete and soundtrack
+complete_and_soundtrack %>%
+  identical(definitive)
+
+
+# Check if same, rows in any order: definitive and union of complete and soundtrack
+complete_and_soundtrack %>%
+  setequal(definitive)
+
+# Examine side_one and side_two
+side_one
+side_two
+
+# Bind side_one and side_two into a single dataset
+side_one %>% 
+  bind_rows(side_two)
+
+# Examine discography and jimi
+discography
+jimi
+
+jimi %>% 
+  # Bind jimi by rows, with ID column "album"
+  bind_rows(.id = "album") %>% 
+  # Make a complete data frame
+  left_join(discography)
+
+# Examine hank_years and hank_charts
+hank_years
+hank_charts
+
+hank_years %>% 
+  # Reorder hank_years alphabetically by song title
+  arrange(song) %>% 
+  # Select just the year column
+  select(year) %>% 
+  # Bind the year column
+  bind_cols(hank_charts) %>% 
+  # Arrange the finished dataset
+  arrange(year)
+
+# Make combined data frame using data_frame()
+data_frame(year = hank_year, song = hank_song, peak = hank_peak) %>% 
+  # Extract songs where peak equals 1
+  filter(peak == 1)
+
+# Examine the contents of hank
+hank
+
+# Convert the hank list into a data frame
+as_data_frame(hank) %>% 
+  # Extract songs where peak equals 1
+  filter(peak == 1)
+
+# Examine the contents of michael
+michael
+
+# Replace the first line so each album has its own rows
+as_data_frame(bind_rows(michael, .id = "album")) %>% 
+  group_by(album) %>% 
+  mutate(rank = min_rank(peak)) %>% 
+  filter(rank == 1) %>% 
+  select(-rank, -peak)
+
+# Coertion
+seventies %>% 
+  # Coerce seventies$year into a useful numeric
+  mutate(year = as.numeric(as.character(year))) %>% 
+  # Bind the updated version of seventies to sixties
+  bind_rows(sixties) %>% 
+  arrange(year)
+
+# To add column names to an existing data frame
+# Load the tibble package
+library(tibble)
+
+stage_songs %>% 
+  # Add row names as a column named song
+  rownames_to_column(var = "song") %>% 
+  # Left join stage_writers to stage_songs
+  left_join(stage_writers)
+
+# Examine the result of joining singers to two_songs
+two_songs %>% inner_join(singers, by = "movie")
+
+# Remove NA's from key before joining
+two_songs %>% 
+  filter(!is.na(movie)) %>% 
+  inner_join(singers, by = "movie")
+
+# Identifying key columns to join data
+movie_years %>% 
+  # Left join movie_studios to movie_years
+  left_join(movie_studios, by = "movie") %>% 
+  # Rename the columns: artist and studio
+  rename(artist = name.x, studio = name.y)
+
+# Identify the key column
+elvis_songs
+elvis_movies
+
+elvis_movies %>% 
+  # Left join elvis_songs to elvis_movies by this column
+  left_join(elvis_songs, by = c("name" = "movie")) %>% 
+  # Rename columns
+  rename(movie = name, song = name.y)
+
+# Identify the key columns
+movie_directors
+movie_years
+
+movie_years %>% 
+  # Left join movie_directors to movie_years
+  left_join(movie_directors, by = c("movie" = "name")) %>% 
+  rename(artist = name) %>%
+  # Arrange the columns using select()
+  select(c(year, movie, artist, director, studio))
+
+# purrr's reduce() function is very useful for joining together multiple datasets.
+# Load the purrr library
+library(purrr)
+
+# Place supergroups, more_bands, and more_artists into a list
+list(supergroups, more_bands, more_artists) %>% 
+  # Use reduce to join together the contents of the list
+  reduce(left_join)
+
+list(more_artists, more_bands, supergroups) %>% 
+  # Return rows of more_artists in all three datasets
+  reduce(semi_join)
+
+# Alter the code to perform the join with a dplyr function (original)
+merge(bands, artists, by = c("first", "last"), all.x = TRUE) %>%
+  arrange(band)
+
+# Alter the code to perform the join with a dplyr function
+left_join(bands, artists, by = c("first", "last")) %>%
+  arrange(band)
+
+# Examine lahmanNames
+lahmanNames
+
+# Find variables in common
+reduce(lahmanNames, intersect, by = "var")
+
+lahmanNames %>%  
+  # Bind the data frames in lahmanNames
+  bind_rows(.id = "dataframe") %>%
+  # Group the result by var
+  group_by(var) %>%
+  # Tally the number of appearances
+  tally() %>%
+  # Filter the data
+  filter(n > 1) %>% 
+  # Arrange the results
+  arrange(desc(n))
+
+# Extracting playerID
+lahmanNames %>% 
+  # Bind the data frames
+  bind_rows(.id = "dataframe") %>%
+  # Filter the results
+  filter(var == "playerID") %>% 
+  # Extract the dataframe variable
+  `$`("dataframe")
+
+#  distinct() to find unique rows for all columns in a table.
+# tbl %>% 
+#Find unique rows of columns a,b, and c
+#  distinct(a, b, c)
+players <- Master %>% 
+  # Return one row for each distinct player
+  distinct(playerID, nameFirst, nameLast)
+  
+players %>% 
+  # Find all players who do not appear in Salaries
+  anti_join(Salaries, by = "playerID") %>%
+  # Count them
+  count()
+
+players %>% 
+  anti_join(Salaries, by = "playerID") %>% 
+  # How many unsalaried players appear in Appearances?
+  semi_join(Appearances) %>% 
+  count()
+
+players %>% 
+  # Find all players who do not appear in Salaries
+  anti_join(Salaries, by = "playerID") %>% 
+  # Join them to Appearances
+  left_join(Appearances, by = "playerID") %>% 
+  # Calculate total_games for each player
+  group_by(playerID) %>%
+  summarize(total_games = sum(G_all)) %>%
+  # Arrange in descending order by total_games
+  arrange(desc(total_games))
+
+players %>%
+  # Find unsalaried players
+  anti_join(Salaries, by = "playerID") %>% 
+  # Join Batting to the unsalaried players
+  left_join(Batting) %>% 
+  # Group by player
+  group_by(playerID) %>% 
+  # Sum at-bats for each player
+  summarize(total_at_bat = sum(AB)) %>% 
+  # Arrange in descending order
+  arrange(desc(total_at_bat))
+
+# Find the distinct players that appear in HallOfFame
+nominated <- HallOfFame %>% 
+  distinct(playerID)
+
+nominated %>% 
+  # Count the number of players in nominated
+  count()
+
+nominated_full <- nominated %>% 
+  # Join to Master
+  left_join(Master, by = "playerID") %>% 
+  # Return playerID, nameFirst, nameLast
+  distinct(playerID, nameFirst, nameLast)
+
+# Find distinct players in HallOfFame with inducted == "Y"
+inducted <- HallOfFame %>% 
+  filter(inducted == "Y") %>%
+  distinct(playerID)
+
+inducted %>% 
+  # Count the number of players in inducted
+  count()
+
+inducted_full <- inducted %>% 
+  # Join to Master
+  left_join(Master, by = "playerID") %>% 
+  # Return playerID, nameFirst, nameLast
+  distinct(playerID, nameFirst, nameLast)
+
+# Tally the number of awards in AwardsPlayers by playerID
+nAwards <- AwardsPlayers %>% 
+  group_by(playerID) %>% 
+  tally()
+
+nAwards %>% 
+  # Filter to just the players in inducted 
+  semi_join(inducted, by = "playerID") %>% 
+  # Calculate the mean number of awards per player
+  summarize(avg_n = mean(n, na.rm = TRUE))
+
+nAwards %>% 
+  # Filter to just the players in nominated 
+  semi_join(nominated, by = "playerID") %>%
+  # Filter to players NOT in inducted 
+  anti_join(inducted, by = "playerID") %>%
+  # Calculate the mean number of awards per player
+  summarize(avg_n = mean(n, na.rm = TRUE))
+
+# Find the players who are in nominated, but not inducted
+notInducted <- nominated %>% 
+  setdiff(inducted)
+
+Salaries %>% 
+  # Find the players who are in notInducted
+  semi_join(notInducted, by = "playerID") %>%
+  # Calculate the max salary by player
+  group_by(playerID) %>% 
+  summarize(max_salary = max(salary, na.rm = TRUE)) %>% 
+  # Calculate the average of the max salaries
+  summarize(avg_salary = mean(max_salary, na.rm = TRUE))
+
+# Repeat for players who were inducted
+Salaries %>% 
+  semi_join(inducted, by = "playerID") %>% 
+  group_by(playerID) %>% 
+  summarize(max_salary = max(salary, na.rm = TRUE)) %>% 
+  summarize(avg_salary = mean(max_salary, na.rm = TRUE))
+
+Appearances %>% 
+  # Filter Appearances against nominated
+  semi_join(nominated, by = "playerID") %>% 
+  # Find last year played by player
+  group_by(playerID) %>% 
+  summarize(last_year = max(yearID)) %>% 
+  # Join to full HallOfFame
+  left_join(HallOfFame, by = "playerID") %>% 
+  # Filter for unusual observations
+  filter(last_year >= yearID)
 
