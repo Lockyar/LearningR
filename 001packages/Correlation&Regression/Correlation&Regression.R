85d4ff1d59edb8e9a083ddb6d5d647ba84565a60
Lockyar <1043293+Lockyar@users.noreply.github.com>
2019-04-04 20:31:20 -0500

    Correlation&Regression

---
+++ 001packages/Correlation&Regression/Correlation&Regression.R
@@ -0,0 +1,235 @@
+# Chapter 1
+# Scatterplot of weight vs. weeks
+str(ncbirths)
+ggplot(ncbirths, aes(x = weeks, y = weight)) +
+  geom_point()
+
+# Boxplot of weight vs. weeks
+ggplot(data = ncbirths, 
+       aes(x = cut(weeks, breaks = 5), y = weight)) + 
+  geom_boxplot()
+
+# Mammals scatterplot
+str(mammals)
+ggplot(mammals, aes(x = BodyWt, y = BrainWt)) +
+  geom_point()
+
+# Baseball player scatterplot
+str(mlbBat10)
+ggplot(mlbBat10, aes(x = OBP, y = SLG)) +
+  geom_point()
+
+# Body dimensions scatterplot
+str(bdims)
+ggplot(bdims, aes(x = hgt, y = wgt, col = factor(sex))) +
+  geom_point()
+
+# Smoking scatterplot
+str(smoking)
+ggplot(smoking, aes(x = age, y = amtWeekdays)) +
+  geom_point()
+
+# Scatterplot with coord_trans()
+ggplot(data = mammals, aes(x = BodyWt, y = BrainWt)) +
+  geom_point() + 
+  coord_trans(x = "log10", y = "log10")
+
+# Scatterplot with scale_x_log10() and scale_y_log10()
+#  to achieve the same effect but with different axis labels and grid lines.
+ggplot(data = mammals, aes(x = BodyWt, y = BrainWt)) +
+  geom_point() +
+  scale_x_log10() + 
+  scale_y_log10()
+
+# Scatterplot of SLG vs. OBP
+mlbBat10 %>%
+  filter(AB >= 200) %>%
+  ggplot(aes(x = OBP, y = SLG)) +
+  geom_point()
+
+# Identify the outlying player
+mlbBat10 %>%
+  filter(AB >= 200, OBP < 0.2)
+
+# Chapter 2
+# Compute correlation
+ncbirths %>%
+  summarize(N = n(), r = cor(weight, mage))
+
+# Compute correlation for all non-missing pairs
+# Setting the use argument to "pairwise.complete.obs" allows cor() to compute
+# the correlation coefficient for those observations where the values of x and y are both not missing.
+ncbirths %>%
+  summarize(N = n(), r = cor(weight, weeks, use = "pairwise.complete.obs"))
+
+# Compute properties of Anscombe
+Anscombe %>%
+  group_by(set) %>%
+  summarize(N = n(), mean(x), sd(x), mean(y), sd(y), cor(x, y))
+
+# Correlation for all baseball players
+mlbBat10 %>%
+  summarize(N = n(), r = cor(OBP, SLG))
+
+# Correlation for all players with at least 200 ABs
+mlbBat10 %>%
+  filter(AB >= 200) %>%
+  summarize(N = n(), r = cor(OBP, SLG))
+
+# Correlation of body dimensions
+bdims %>%
+  group_by(sex) %>%
+  summarize(N = n(), r = cor(hgt, wgt))
+
+# Correlation among mammals, with and without log
+mammals %>%
+  summarize(N = n(), 
+            r = cor(BrainWt, BodyWt), 
+            r_log = cor(log(BrainWt), log(BodyWt)))
+
+# Create faceted scatterplot
+ggplot(noise, aes(x = x, y = y)) +
+  geom_point() +
+  facet_wrap(~ z)
+
+# Compute correlations for each dataset
+noise_summary <- noise %>%
+  group_by(z) %>%
+  summarize(N = n(), spurious_cor = cor(x, y))
+
+# Isolate sets with correlations above 0.2 in absolute strength
+noise_summary %>%
+  filter(abs(spurious_cor) > 0.2)
+
+# Chapter 3
+# Scatterplot with regression line
+ggplot(data = bdims, aes(x = hgt, y = wgt)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se = FALSE)
+
+# Estimate optimal value of my_slope
+add_line(my_slope = 1)
+
+# Print bdims_summary
+bdims_summary
+
+# Add slope and intercept
+bdims_summary %>%
+  mutate(slope = r * sd_wgt / sd_hgt, 
+         intercept = mean_wgt - slope * mean_hgt)
+
+# Height of children vs. height of father
+ggplot(data = Galton_men, aes(x = father, y = height)) +
+  geom_point() + 
+  geom_abline(slope = 1, intercept = 0) + 
+  geom_smooth(method = "lm", se = FALSE)
+
+# Height of children vs. height of mother
+ggplot(data = Galton_women, aes(x = mother, y = height)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0) +
+  geom_smooth(method = "lm", se = FALSE)
+
+# Chapter 4
+# Linear model for weight as a function of height
+lm(wgt ~ hgt, data = bdims)
+
+# Linear model for SLG as a function of OBP
+lm(SLG ~ OBP, data = mlbBat10)
+
+# Log-linear model for body weight as a function of brain weight
+lm(log(BodyWt) ~ log(BrainWt), data = mammals)
+
+# Show the coefficients
+coef(mod)
+
+# Show the full output
+summary(mod)
+
+# Mean of weights equal to mean of fitted values?
+mean(bdims$wgt) == mean(fitted.values(mod))
+
+# Mean of the residuals
+mean(residuals(mod))
+
+# Load broom
+library(broom)
+
+# Create bdims_tidy
+bdims_tidy = augment(mod)
+
+# Glimpse the resulting data frame
+glimpse(bdims_tidy)
+
+# Print ben
+ben
+
+# Predict the weight of ben
+predict(mod, ben)
+
+# Add the line to the scatterplot
+ggplot(data = bdims, aes(x = hgt, y = wgt)) + 
+  geom_point() + 
+  geom_abline(data = coefs, 
+              aes(intercept = `(Intercept)`, slope = hgt),  
+              color = "dodgerblue")
+
+# Chapter 5
+# View summary of model
+summary(mod)
+
+# Compute the mean of the residuals
+mean(residuals(mod))
+
+# Compute RMSE
+sqrt(sum(residuals(mod)^2) / df.residual(mod))
+
+# View model summary
+summary(mod)
+
+# Compute R-squared
+bdims_tidy %>%
+  summarize(var_y = var(wgt), var_e = var(.resid)) %>%
+  mutate(R_squared = 1 - (var_e / var_y))
+
+# Compute Sum of the Squared Residuals (SSE) for null model
+mod_null %>%
+  summarize(SSE = var(.resid))
+
+# Compute Sum of the Squared Residuals (SSE) for regression model
+mod_hgt %>%
+  summarize(SSE = var(.resid))
+
+# Rank points of high leverage .hat in the augment(mod)
+mod %>%
+  augment() %>%
+  arrange(desc(.hat)) %>%
+  head()
+
+# Rank influential points .cooksd in the augment(mod)
+mod %>%
+  augment() %>%
+  arrange(desc(.cooksd)) %>%
+  head()
+
+# Create nontrivial_players
+nontrivial_players <- mlbBat10 %>%
+  filter(AB >= 10, OBP < 0.5)
+
+# Fit model to new data
+mod_cleaner <- lm(SLG ~ OBP, data = nontrivial_players)
+
+# View model summary
+summary(mod_cleaner)
+
+# Visualize new model
+ggplot(data = nontrivial_players, aes(x = OBP, y = SLG)) +
+  geom_point() + 
+  geom_smooth(method = "lm")
+
+# Rank high leverage points
+mod %>%
+  augment() %>%
+  arrange(desc(.hat, .cooksd)) %>%
+  head()
+
