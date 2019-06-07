0289b99de4efd72f2bb68c601a4a02e6e5fc3386
Lockyar <1043293+Lockyar@users.noreply.github.com>
2019-06-07 16:45:23 -0500

    Credit Risk Modeling in R

--- Quantitative Analyst with R/Credit Risk Modeling in R/Credit Risk Modeling in R.R
+++ Quantitative Analyst with R/Credit Risk Modeling in R/Credit Risk Modeling in R.R
@@ -327,3 +327,81 @@ plot(strategy_loss_matrix$accept_rate, strategy_loss_matrix$bad_rate,
      type = "l", xlab = "Acceptance rate", 
      ylab = "Bad rate", lwd = 2, main = "tree")
 
+# Load the pROC-package
+library(pROC)
+
+# Construct the objects containing ROC-information
+ROC_logit <- roc(test_set$loan_status, predictions_logit)
+ROC_probit <- roc(test_set$loan_status, predictions_probit)
+ROC_cloglog <- roc(test_set$loan_status, predictions_cloglog)
+ROC_all_full <- roc(test_set$loan_status, predictions_all_full)
+
+# Draw all ROCs on one plot
+plot(ROC_logit)
+lines(ROC_probit, col= "blue")
+lines(ROC_cloglog, col= "red")
+lines(ROC_all_full, col= "green")
+
+# Compute the AUCs
+auc(ROC_logit)
+auc(ROC_probit)
+auc(ROC_cloglog)
+auc(ROC_all_full)
+
+# Construct the objects containing ROC-information
+ROC_undersample <- roc(test_set$loan_status, predictions_undersample)
+ROC_prior <- roc(test_set$loan_status, predictions_prior)
+ROC_loss_matrix <- roc(test_set$loan_status, predictions_loss_matrix)
+ROC_weights <- roc(test_set$loan_status, predictions_weights)
+
+# Draw the ROC-curves in one plot
+plot(ROC_undersample)
+lines(ROC_prior, col="blue")
+lines(ROC_loss_matrix, col="red")
+lines(ROC_weights, col="green")
+
+# Compute the AUCs
+auc(ROC_undersample)
+auc(ROC_prior)
+auc(ROC_loss_matrix)
+auc(ROC_weights)
+
+# Build four models each time deleting one variable in log_3_remove_ir
+log_4_remove_amnt <- glm(loan_status ~ grade + annual_inc + emp_cat, 
+                        family = binomial, data = training_set) 
+log_4_remove_grade <- glm(loan_status ~ annual_inc + emp_cat, 
+                        family = binomial, data = training_set)
+log_4_remove_inc <- glm(loan_status ~ grade + emp_cat, 
+                        family = binomial, data = training_set)
+log_4_remove_emp <- glm(loan_status ~ grade + annual_inc, 
+                        family = binomial, data = training_set)
+
+# Make PD-predictions for each of the models
+pred_4_remove_amnt <- predict(log_4_remove_amnt, newdata = test_set, type = "response")
+pred_4_remove_grade <- predict(log_4_remove_grade, newdata = test_set, type = "response")
+pred_4_remove_inc <- predict(log_4_remove_inc, newdata = test_set, type = "response")
+pred_4_remove_emp <- predict(log_4_remove_emp, newdata = test_set, type = "response")
+
+# Compute the AUCs
+auc(test_set$loan_status, pred_4_remove_amnt)
+auc(test_set$loan_status, pred_4_remove_grade)
+auc(test_set$loan_status, pred_4_remove_inc)
+auc(test_set$loan_status, pred_4_remove_emp)
+
+# Build three models each time deleting one variable in log_4_remove_amnt
+log_5_remove_grade <- glm(loan_status ~ annual_inc + emp_cat, family = binomial, data = training_set)  
+log_5_remove_inc <- glm(loan_status ~ grade  + emp_cat , family = binomial, data = training_set)
+log_5_remove_emp <- glm(loan_status ~ grade + annual_inc, family = binomial, data = training_set)
+
+# Make PD-predictions for each of the models
+pred_5_remove_grade <- predict(log_5_remove_grade, newdata = test_set, type = "response")
+pred_5_remove_inc <- predict(log_5_remove_inc, newdata = test_set, type = "response")
+pred_5_remove_emp <- predict(log_5_remove_emp, newdata = test_set, type = "response")
+
+# Compute the AUCs
+auc(test_set$loan_status, pred_5_remove_grade)
+auc(test_set$loan_status, pred_5_remove_inc)
+auc(test_set$loan_status, pred_5_remove_emp)
+
+# Plot the ROC-curve for the best model here
+plot(roc(test_set$loan_status, pred_4_remove_amnt))
