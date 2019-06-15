7ed5332b3bf00124a7450aea862359db15017be4
Lockyar <1043293+Lockyar@users.noreply.github.com>
2019-06-15 09:46:15 -0500

    Writing Efficient R Code

---
+++ 001packages/Writing Efficient R Code/Writing Efficient R Code.R
@@ -0,0 +1,248 @@
+## Chapter 1
+# Print the R version details using version
+version
+
+# Assign the variable major to the major component
+major <- version$major
+
+# Assign the variable minor to the minor component
+minor <- version$minor
+
+# Reading in RDS is much quicker than CSV
+# How long does it take to read movies from CSV?
+system.time(read.csv("movies.csv"))
+
+# How long does it take to read movies from RDS?
+system.time(readRDS("movies.rds"))
+
+# Load the microbenchmark package
+library(microbenchmark)
+
+# Compare the two functions
+# times = 10 to call each function 10 times
+compare <- microbenchmark(read.csv("movies.csv"), 
+                          readRDS("movies.rds"), 
+                          times = 10)
+
+# Print compare
+compare
+
+# Load the benchmarkme package
+library(benchmarkme)
+
+# Assign the variable ram to the amount of RAM on this machine
+ram <- get_ram()
+ram
+
+# Assign the variable cpu to the cpu specs
+cpu <- get_cpu()
+cpu
+
+# Load the package
+library("benchmarkme")
+
+# Run the io benchmark
+# records the length of time it takes to read and write a 5MB file.
+res <- benchmark_io(runs = 1, size = 5)
+
+# Plot the results
+plot(res)
+
+## Chapter 2
+# Slow code
+n <- 30000
+# Slow code
+growing <- function(n) {
+    x <- NULL
+    for(i in 1:n)
+        x <- c(x, rnorm(1))
+    x
+}
+
+# Use <- with system.time() to store the result as res_grow
+system.time(res_grow <- growing(n = 30000))
+
+# Faster code
+n <- 30000
+# Fast code
+pre_allocate <- function(n) {
+    x <- numeric(n) # Pre-allocate
+    for(i in 1:n) 
+        x[i] <- rnorm(1)
+    x
+}
+
+
+# Use <- with system.time() to store the result as res_allocate
+n <- 30000
+system.time(res_allocate <- pre_allocate(n))
+
+# Vectorizing this code to make it faster 
+x <- rnorm(10)
+x2 <- numeric(length(x))
+for(i in 1:10)
+    x2[i] <- x[i] * x[i]
+
+
+# Store your answer as x2_imp
+x2_imp <- x * x
+
+# Initial code
+n <- 100
+total <- 0
+x <- runif(n)
+for(i in 1:n) 
+    total <- total + log(x[i])
+
+# Rewrite in a single line. Store the result in log_sum
+log_sum <- sum(log(x))
+
+# Which is faster, mat[, 1] or df[, 1]?
+# The matrix is much faster because all values in a matrix object must be the same data type
+microbenchmark(mat[, 1], df[, 1])
+
+# Which is faster, mat[1, ] or df[1, ]? 
+# The matrix is way much faster because the values of a column of a data frame must be the same data type, whereas that of a row doesn't have to be.
+microbenchmark(mat[1, ], df[1, ])
+
+## Chapter 3
+# Load the data set
+data(movies, package = "ggplot2movies") 
+
+# Load the profvis package
+library(profvis)
+
+# Profile the following code with the profvis function
+profvis({
+  # Load and select data
+  movies <- movies[movies$Comedy == 1, ]
+
+  # Plot data of interest
+  plot(movies$year, movies$rating)
+
+  # Loess regression line
+  model <- loess(rating ~ year, data = movies)
+  j <- order(movies$year)
+  
+  # Add a fitted line to the plot
+  lines(movies$year[j], model$fitted[j], col = "red")
+})     ## Remember the closing brackets!
+
+# Load the microbenchmark package
+library(microbenchmark)
+
+# The previous data frame solution is defined
+# d() Simulates 6 dices rolls
+d <- function() {
+  data.frame(
+    d1 = sample(1:6, 3, replace = TRUE),
+    d2 = sample(1:6, 3, replace = TRUE)
+  )
+}
+
+# Complete the matrix solution
+m <- function() {
+  matrix(sample(1:6, 6, replace = TRUE), ncol = 2)
+}
+
+# Use microbenchmark to time m() and d()
+microbenchmark(
+ data.frame_solution = d(),
+ matrix_solution     = m()
+)
+
+# Example data
+rolls
+
+# Define the previous solution 
+app <- function(x) {
+    apply(x, 1, sum)
+}
+
+# Define the new solution
+r_sum <- function(x) {
+    rowSums(x)
+}
+
+# Compare the methods
+microbenchmark(
+    app_sol = app(rolls),
+    r_sum_sol = r_sum(rolls)
+)
+
+# Example data
+is_double
+
+# Define the previous solution
+move <- function(is_double) {
+    if (is_double[1] & is_double[2] & is_double[3]) {
+        current <- 11 # Go To Jail
+    }
+}
+
+# Define the improved solution
+improved_move <- function(is_double) {
+    if (is_double[1] && is_double[2] && is_double[3]) {
+        current <- 11 # Go To Jail
+    }
+}
+
+# microbenchmark both solutions
+# Very occassionally the improved solution is actually a little slower
+# This is just random chance
+microbenchmark(move, improved_move, times = 1e5)
+
+## Chapter 4
+# Load the parallel package
+library(parallel)
+
+# Store the number of cores in the object no_of_cores
+no_of_cores <- detectCores()
+
+# Print no_of_cores
+no_of_cores
+
+# Determine the number of available cores
+detectCores()
+
+# Create a cluster via makeCluster
+cl <- makeCluster(2)
+
+# Parallelize this code
+# apply(dd, 2, median)
+parApply(cl, dd, 2, median)
+
+# Stop the cluster
+stopCluster(cl)
+
+# Load the parallel package
+library("parallel")
+
+# Create a cluster via makeCluster (2 cores)
+cl <- makeCluster(2)
+
+# Export the play() function to the cluster
+clusterExport(cl, "play")
+
+# Re-write sapply as parSapply
+# res <- sapply(1:100, function(i) play())
+res <- parSapply(cl, 1:100, function(i) play())
+
+# Stop the cluster
+stopCluster(cl)
+
+# Set the number of games to play
+no_of_games <- 1e5
+
+## Time serial version
+system.time(serial <- sapply(1:no_of_games, function(i) play()))
+
+## Set up cluster
+cl <- makeCluster(4)
+clusterExport(cl, "play")
+
+## Time parallel version
+system.time(par <- parSapply(cl, 1:no_of_games, function(i) play()))
+
+## Stop cluster
+stopCluster(cl)
